10870ffd6964e73144264da71f8126c11d3214e9
--1stRayos <--emc1140@hotmail.com>
2019-03-24 17:50:40 -0500

    Added necessary functions to to cpp file of Library class

--- Project 1 - Library Employees/Library.cpp
+++ Project 1 - Library Employees/Library.cpp
@@ -1,12 +1,27 @@
 #include "Library.h"
 
 
-
 Library::Library()
 {
 }
 
 
 Library::~Library()
+{
+}
+
+Library::add_book() 
+{
+}
+
+Library::add_employee()
+{
+}
+
+Library::circulate_book()
+{
+}
+
+Library::pass_on()
 {
 }
